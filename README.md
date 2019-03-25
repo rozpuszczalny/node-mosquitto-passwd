f75589efdfb642e1134c2c364bc4a9e36a44cc0a
Tomasz Domański <rozpuszczalny@gmail.com>
2019-03-25 10:26:45 +0100

    Add node version 6 to travis tests Add few badges and rename default import in README.md Change tests so node 6 can handle them Add code coverage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/rozpuszczalny/node-mosquitto-passwd.svg?branch=master)](https://travis-ci.org/rozpuszczalny/node-mosquitto-passwd) [![npm version](https://badge.fury.io/js/mosquitto-passwd.svg)](https://badge.fury.io/js/mosquitto-passwd)
+
 # mosquitto-passwd
 
 Create `mosquitto_passwd` entries using only Node.JS.
@@ -5,15 +7,15 @@ Create `mosquitto_passwd` entries using only Node.JS.
 ## Usage
 
 ```javascript
-const passwd = require('mosquitto-passwd')
+const mosquittoPasswd = require('mosquitto-passwd')
 
 // Username, password, salt (optional, by default will generate 12 bytes salt)
-passwd('123', '123', Buffer.from('fcQNw5IJfhWEEEM4', 'base64')).then(entry => {
+mosquittoPasswd('123', '123', Buffer.from('fcQNw5IJfhWEEEM4', 'base64')).then(entry => {
     console.log(entry) // 123:$6$fcQNw5IJfhWEEEM4$0rxaqu90qMJ0FLRlxCWPZiDFfemkc2UOpCHjUEKZ32C6AcKe9x5QKEFPuZwrW9jqoOmdOfP/3FKvbY48AFpSlA==
 })
 
 // Or use with await
-console.log(await passwd(
+console.log(await mosquittoPasswd(
     '123', 
     '123', 
     Buffer.from('fcQNw5IJfhWEEEM4', 'base64')
--- index.test.js
+++ index.test.js
@@ -2,14 +2,24 @@ const assert = require('assert')
 const mosquitto_passwd = require('./')
 
 describe('mosquitto_passwd', () => {
-    it('should match output of mosquitto_passwd (123:123)', async () => {
-        const res = await mosquitto_passwd('123', '123', Buffer.from('fcQNw5IJfhWEEEM4', 'base64'))
-
-        assert.equal(res, '123:$6$fcQNw5IJfhWEEEM4$0rxaqu90qMJ0FLRlxCWPZiDFfemkc2UOpCHjUEKZ32C6AcKe9x5QKEFPuZwrW9jqoOmdOfP/3FKvbY48AFpSlA==')
+    it('should match output of mosquitto_passwd (123:123)', (done) => {
+        mosquitto_passwd('123', '123', Buffer.from('fcQNw5IJfhWEEEM4', 'base64')).then(res => {
+            assert.equal(res, '123:$6$fcQNw5IJfhWEEEM4$0rxaqu90qMJ0FLRlxCWPZiDFfemkc2UOpCHjUEKZ32C6AcKe9x5QKEFPuZwrW9jqoOmdOfP/3FKvbY48AFpSlA==')
+            done()
+        })
     })
-    it('should match output of mosquitto_passwd (long username and password)', async () => {
-        const res = await mosquitto_passwd('AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12', 'AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12', Buffer.from('2/gNQjlq6sx3Q516', 'base64'))
-
-        assert.equal(res, 'AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12:$6$2/gNQjlq6sx3Q516$E8F2TbpvTPnT2CG74QTL53zx19QbVQzE4gHrB6Bnl3MH1NzOIQRgaqUZFfAIghleSvj2xZbGlJLlZHi2RMWJtw==')
+    it('should match output of mosquitto_passwd (long username and password)', (done) => {
+        mosquitto_passwd('AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12', 'AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12', Buffer.from('2/gNQjlq6sx3Q516', 'base64')).then(res => {
+            assert.equal(res, 'AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12AAAbbbbCCCCddd99a12:$6$2/gNQjlq6sx3Q516$E8F2TbpvTPnT2CG74QTL53zx19QbVQzE4gHrB6Bnl3MH1NzOIQRgaqUZFfAIghleSvj2xZbGlJLlZHi2RMWJtw==')
+            done()
+        })
+    })
+    it('should create salt automaticly', (done) => {
+        mosquitto_passwd('username', 'password').then(res => {
+            assert.equal(res.indexOf('username'), 0)
+            assert.equal(res.indexOf(':$6$'), 8)
+            assert.equal(res.lastIndexOf('$'), 28)
+            done()
+        })
     })
 })
