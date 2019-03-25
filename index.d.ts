ef8ccce4254d49977eb667ecb68b705c75802a08
Tomasz Domański <rozpuszczalny@gmail.com>
2019-03-25 10:09:19 +0100

    Fix typings; add travis

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
+/// <reference types="node" />
+
 declare namespace mosquittoPasswd {
     export interface MosquittoPasswd {
         (username: string, password: string, salt?: Buffer): Promise<string>;
     }
 }
 
-declare const mosquittoPasswd = mosquittoPasswd.MosquittoPasswd;
-
+declare const mosquittoPasswd: mosquittoPasswd.MosquittoPasswd;
 export = mosquittoPasswd;
